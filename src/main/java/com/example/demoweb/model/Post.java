6e6c992588c387d86ed7b5d9d425ce469e87534a
MatveyTolpeev <sergej.zalpov.999@bk.ru>
2020-09-08 23:15:37 +0500

    big commit

--- src/main/java/com/example/demoweb/model/Post.java
+++ src/main/java/com/example/demoweb/model/Post.java
@@ -1,20 +1,28 @@
 package com.example.demoweb.model;
 
 
-public class Post {
-    private String text;
+import java.util.Date;
 
+public class Post {
+
+    private String text;
+    private Date creationDate;
     private Integer likes = 0;
 
     public Integer getLikes() {
         return likes;
     }
 
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
     public String getText() {
         return text;
     }
 
-    public Post(String text) {
+    public Post(String text,Date creationDate) {
         this.text = text;
+        this.creationDate = creationDate;
     }
 }
